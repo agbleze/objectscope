7f2a4011a075fc22708446cd16d6f2abd3990330
linus <lagbleze@st.ug.edu.gh>
2025-07-30 23:59:43 +0200

    feat: add config read

--- src/objectscope/__init__.py
+++ src/objectscope/__init__.py
@@ -16,3 +16,17 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 
 logger.info(f"{package_name} version {__version__} initialized.")
+
+
+train_img_dir = config("TRAIN_IMG_DIR", default=None, cast=str)
+test_img_dir = config("TEST_IMG_DIR", default=None, cast=str)
+train_coco_json_file = config("TRAIN_COCO_JSON_FILE", default=None, cast=str)
+test_coco_json_file = config("TEST_COCO_JSON_FILE", default=None, cast=str)
+output_dir = config("OUTPUT_DIR", default="output/object_detector", cast=str)
+num_classes = config("NUM_CLASSES", default=None, cast=int)
+num_workers = config("NUM_WORKERS", default=None, cast=int)
+imgs_per_batch = config("IMGS_PER_BATCH", default=4, cast=int)
+base_lr = config("BASE_LR", default=0.00005, cast   =float)
+max_iter = config("MAX_ITER", default=5000, cast=int)
+checkpoint_period = config("CHECKPOINT_PERIOD", default=50, cast=int)
+device = config("DEVICE", default="cuda", cast=str)
