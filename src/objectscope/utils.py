63306b80bb761492d10b717aa4cb7e7bb3ccc670
linus <lagbleze@st.ug.edu.gh>
2025-08-10 12:25:43 +0200

    feat: add run_optimize_model

--- src/objectscope/utils.py
+++ src/objectscope/utils.py
@@ -1,6 +1,7 @@
 from tensorboard import program
 from objectscope import logger
-
+import subprocess
+import os
 
 def launch_tensorboard(logdir, port=None):
     if not port:
@@ -10,3 +11,22 @@ def launch_tensorboard(logdir, port=None):
     tb.configure(argv)
     url = tb.lauch()
     logger.info(f"TensorBoard launched at {url}")
+    
+    
+def run_optimize_model(model_name_or_path, output_dir, device="cpu",
+                       provider="CPUExecutionProvider",
+                       precision="int4",
+                       ):
+    logger.info("Optimizing model...")
+    cmd = ["olive", "auto-opt",
+            "--model_name_or_path", model_name_or_path,
+            "--trust_remote_code",
+            "--output_path", output_dir,
+            "--device", device,
+            "--provider", provider,
+            "--use_ort_genai",
+            "--precision", precision,
+            "--log_level", 1
+            ]
+    subprocess.run(cmd, check=True)
+    logger.info("Model optimization completed.")
