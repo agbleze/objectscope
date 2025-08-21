2b498b8974596dd22712c14bcadcafd0078b55bd
linus <lagbleze@st.ug.edu.gh>
2025-08-21 23:47:45 +0200

    fix: update OnnxModelExporter has no inputs attribute

--- src/objectscope/model_export_utils.py
+++ src/objectscope/model_export_utils.py
@@ -9,6 +9,7 @@ from detectron2.export import scripting_with_instances
 from torch import Tensor
 from detectron2.structures import Boxes
 from typing import Literal
+from torch.onnx import TrainingMode
 
 fields = {"proposal_boxes": Boxes,
     "objectness_logits": Tensor,
@@ -44,7 +45,7 @@ class OnnxModelExporter(object):
                 inputs = self.inputs
             else:
                 inputs = self.get_sample_model_inputs()
-        self.wrapper = TracingAdapter(model, inputs=inputs)
+        self.wrapper = TracingAdapter(model.eval(), inputs=inputs)
         return self.wrapper
     
     def get_predictor(self, cfg=None):
@@ -68,18 +69,47 @@ class OnnxModelExporter(object):
         return self.inputs
     
     def export_to_onnx(self, save_onnx_as, inputs=None,
-                        model=None
+                        model=None, 
+                        torchscript_formart: Literal["trace", "script"] = "script"
                         ):
-        traced_model = self.get_traceadapted_model(model=model,
-                                                   inputs=inputs
-                                                   )
-        inputs = inputs if inputs else self.inputs
+        if torchscript_formart not in ["trace", "script"]:
+            raise ValueError(f"torchscript_formart has to be either trace or script not {torchscript_formart}")
+        if not model:
+            if torchscript_formart == "trace":
+                if hasattr(self, "wrapper"):
+                    model = self.wrapper
+                else:
+                    model = self.get_traceadapted_model(inputs=inputs)
+                #model.eval()
+            else:
+                if hasattr(self, "scripted_model"):
+                    model = self.scripted_model
+                else:
+                    model = self.create_script_model()
+            #model.eval() #.train(True)  # hack to make conversion to onnx possible since onnx turns train off internally
+        # traced_model = self.get_traceadapted_model(model=model,
+        #                                            inputs=inputs
+        #                                            )
+        #inputs = inputs if inputs else self.
+        if not inputs:
+            if hasattr(self, "inputs"):
+                inputs = self.inputs
+            else:
+                inputs = self.get_sample_model_inputs()
             
         with open(save_onnx_as, "wb") as f:
             image = inputs[0]["image"]
-            torch.onnx.export(model=traced_model.eval(),
+            torch.onnx.export(model=model,
                             args = (image,),
-                            f = f, opset_version=16
+                            f = f, opset_version=16,
+                            export_params=True,
+                            training=TrainingMode.PRESERVE,
+                            do_constant_folding=True,
+                            dynamic_axes={
+                                "input": {0: "batch_size"},
+                                #"image": {0: "batch_size", 2: "height", 3: "width"},
+                                "output": {0: "batch_size"}
+                                }
                             )
         logger.info(f"Successfully exported model to onnx at: {save_onnx_as}")
         
@@ -112,5 +142,5 @@ class OnnxModelExporter(object):
                                     )
         else:
             model = self.create_script_model(model=None)
-            model.eval()
+            
         model.save(save_model_as)
