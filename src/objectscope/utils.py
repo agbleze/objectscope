e19032954d8ea7a2b36447b8b0929abf33c7769e
linus <lagbleze@st.ug.edu.gh>
2025-08-23 18:58:06 +0200

    feat: add draw_bbox_and_polygons

--- src/objectscope/utils.py
+++ src/objectscope/utils.py
@@ -5,6 +5,7 @@ from PIL import Image
 import numpy as np
 import onnxruntime as ort
 from PIL import Image, ImageDraw, ImageFont
+from cpauger.visualize import random_color
 
 def launch_tensorboard(logdir, port_num=None):
     if not port_num:
@@ -72,4 +73,51 @@ def predict_bbox(image, model_path):
             "score": output[2],
             "shape": output[3],
             }
+
+
+def draw_bbox_and_polygons(image, bboxes, scores, 
+                            class_names=None, 
+                            score_thresh=0.3,
+                            polygons=None
+                        ) -> Image:
+    """Visualize the bbox(es) and segmentation mask(s) of objects in image(s)
+
+    Args:
+        image (Image): PIL Image to draw bbox and segmentation on.
+        bboxes (List): List of boundary boxes.
+        scores (List): Confidence score to use for filtering and drawing on image.
+        class_names (List): Name(s) of objects for each bbox and polygon.
+        score_thresh (float, optional): Threshold to filter out low-confidence detections. Defaults to 0.3.
+        polygons (List, optional): List of segmentation masks. Defaults to None.
+
+    Returns: PIL Image with drawn bbox and segmentation mask.
+    """
+    try:
+        font = ImageFont.truetype("arial.ttf", size=20)
+    except IOError:
+        font = ImageFont.load_default()
+
+    if polygons:
+        image = image.convert("RGBA")
+        mask_img = Image.new("RGBA", image.size)
+        draw = ImageDraw.Draw(mask_img)
+    else:
+        draw = ImageDraw.Draw(image)
+        
+    for bbox, class_name, score, polygon in zip(bboxes, class_names, scores, polygons if polygons else [None]*len(bboxes)):
+        if score < score_thresh:
+            continue
+        color = random_color()
+        bbox = [bbox[0], bbox[1], bbox[2], bbox[3]] #bbox[0]+bbox[2], bbox[1]+bbox[3]]
+        draw.rectangle(bbox, outline=color, width=2)
+        if polygon:
+            draw.polygon(polygon, outline=color, fill=color + (100,))
+        text_position = (bbox[0], bbox[1] - 10)
+        draw.text(text_position, f"{class_name} @ {round(float(score), 4)}", 
+                  fill=color, font=font,
+                  )
+    if polygons:
+        blended_img = Image.alpha_composite(image, mask_img)
+        image = blended_img.convert("RGB")
+    return image    
     
